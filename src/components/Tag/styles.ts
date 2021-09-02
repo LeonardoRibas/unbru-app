a84ad3da6b00806ce2a360f357f63e77e170bd84
Leo Ribas <leonas.ribas@gmail.com>
2021-09-01 23:28:37 -0300

    ✨ Adiciona componente de Tag

---
+++ src/components/Tag/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        borderRadius: 8,
+        backgroundColor: "#3AB449",
+    },
+    text: {
+        fontFamily: "Lexend_600SemiBold",
+        fontSize: 13,
+        color: "white",
+        marginLeft: 8,
+    },
+});
+
+export default styles;
