329cb999228b61e8e9476e63c11354f733190484
bruno <brunofvteixeira@gmail.com>
2021-11-21 17:23:10 -0300

    feat: implementa header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,74 +1,36 @@
 import React from "react";
 import dayjs from "dayjs";
 import styles from "./styles";
-import { Text } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
-import { RectButton } from "react-native-gesture-handler";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import { MaterialTopTabBarProps } from "@react-navigation/material-top-tabs";
 
 type HeaderProps = MaterialTopTabBarProps & {
     title: string;
 };
 
-export default function Header({ state, navigation, title }: HeaderProps): React.ReactElement {
+export default function Header({ navigation, title }: HeaderProps): React.ReactElement {
     const yesterday = dayjs().subtract(1, "day").format("DD/MM");
     const today = dayjs().format("DD/MM");
     const tomorrow = dayjs().add(1, "day").format("DD/MM");
 
     return (
-        <SafeAreaView style={styles.container}>
-            {state.index - 1 >= 0 && (
-                <RectButton
-                    style={styles.leftButtonWrapper}
-                    onPress={() =>
-                        navigation.navigate(
-                            state.routeNames[state.index - 1 >= 0 ? state.index - 1 : 0]
-                        )
-                    }
-                >
-                    <MaterialIcons name="keyboard-arrow-left" size={24} color="#7E7E7E" />
-                    <Text style={styles.buttonTextLeft}>
-                        {title === today
-                            ? "Ontem"
-                            : title === tomorrow
-                            ? "Hoje"
-                            : state.routeNames[state.index - 1]}
-                    </Text>
-                </RectButton>
-            )}
-            <Text style={styles.title}>
-                {title === today
-                    ? "Hoje"
-                    : title === tomorrow
-                    ? "Amanhã"
-                    : title === yesterday
-                    ? "Ontem"
-                    : title}
-            </Text>
-            {state.index + 1 <= state.routeNames.length - 1 && (
-                <RectButton
-                    style={styles.rightButtonWrapper}
-                    onPress={() =>
-                        navigation.navigate(
-                            state.routeNames[
-                                state.index + 1 <= state.routeNames.length - 1
-                                    ? state.index + 1
-                                    : state.routeNames.length - 1
-                            ]
-                        )
-                    }
-                >
-                    <Text style={styles.buttonTextRight}>
-                        {title === today
-                            ? "Amanhã"
-                            : title === yesterday
-                            ? "Hoje"
-                            : state.routeNames[state.index + 1]}
-                    </Text>
-                    <MaterialIcons name="keyboard-arrow-right" size={24} color="#7E7E7E" />
-                </RectButton>
-            )}
-        </SafeAreaView>
+        <>
+          <View style={styles.safeAreaContainer}>
+          </View>
+          <View style={styles.container}>
+              <Feather name="settings" color="white" size={22}/>
+              <Text style={styles.title}>
+                  {title === today
+                      ? "Hoje"
+                      : title === tomorrow
+                      ? "Amanhã"
+                      : title === yesterday
+                      ? "Ontem"
+                      : title}
+              </Text>
+              <Feather name="calendar" color="white" size={22}/>
+          </View>
+        </>
     );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,42 +1,21 @@
 import Constants from "expo-constants";
 import { StyleSheet } from "react-native";
 import * as colors from "../../styles/colors";
+import { Sizing } from '../../styles';
 
 const styles = StyleSheet.create({
+    safeAreaContainer: {
+      height: Constants.statusBarHeight,
+      backgroundColor: colors.primary.brand,
+    },
     container: {
-        justifyContent: "center",
+        flexDirection: "row",
+        justifyContent: "space-between",
         alignItems: "center",
         width: "100%",
-        height: 56 + Constants.statusBarHeight,
+        height: Sizing.layout.x70,
         backgroundColor: colors.primary.brand,
-    },
-    leftButtonWrapper: {
-        position: "absolute",
-        left: 16,
-        bottom: 0,
-        flexDirection: "row",
-        alignItems: "center",
-        paddingVertical: 4,
-    },
-    rightButtonWrapper: {
-        position: "absolute",
-        right: 16,
-        bottom: 0,
-        flexDirection: "row",
-        alignItems: "center",
-        paddingVertical: 4,
-    },
-    buttonTextLeft: {
-        fontFamily: "Lexend_500Medium",
-        fontSize: 13,
-        color: colors.neutral.white,
-        marginRight: 10,
-    },
-    buttonTextRight: {
-        fontFamily: "Lexend_500Medium",
-        fontSize: 13,
-        marginLeft: 10,
-        color: colors.neutral.white,
+        paddingHorizontal: Sizing.layout.x30,
     },
     title: {
         fontFamily: "Lexend_700Bold",
