13adce2600e334c33d8af5a5e9847ee3ea912412
Leo Ribas <leonas.ribas@gmail.com>
2021-11-23 01:20:21 -0300

    🎨 Melhora a padronização da estilização do Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,26 +1,22 @@
-import Constants from "expo-constants";
 import { StyleSheet } from "react-native";
-import * as colors from "../../styles/colors";
-import { Sizing } from '../../styles';
+import { Sizing, Typography, Colors } from "../../styles";
 
 const styles = StyleSheet.create({
-    safeAreaContainer: {
-      height: Constants.statusBarHeight,
-      backgroundColor: colors.primary.brand,
-    },
     container: {
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        width: "100%",
-        height: Sizing.layout.x70,
-        backgroundColor: colors.primary.brand,
-        paddingHorizontal: Sizing.layout.x30,
+        backgroundColor: Colors.primary.brand,
+        paddingHorizontal: Sizing.margin.base,
+        paddingVertical: Sizing.layout.x20,
     },
     title: {
-        fontFamily: "Lexend_700Bold",
-        fontSize: 20,
-        color: colors.neutral.white,
+        fontFamily: Typography.fontWeight.bold,
+        fontSize: Typography.fontSize.x30,
+        color: Colors.neutral.white,
+    },
+    icon: {
+        padding: 4,
     },
 });
 
