20c1070e8d9eef7aa4c488b1c66962ec847c5ceb
Leo Ribas <leonas.ribas@gmail.com>
2021-12-25 17:04:46 -0300

    ✨ Cria componente para exibir o ActivityIndicator

---
+++ src/components/ActivityIndicatorBox/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, ActivityIndicator } from "react-native";
+import styles from "./styles";
+import { Colors } from "../../styles";
+
+export default function ActivityIndicatorBox(): React.ReactElement {
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator size="large" color={Colors.primary.brand} />
+        </View>
+    );
+}
