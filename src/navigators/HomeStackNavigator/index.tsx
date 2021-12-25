9a6b78c9c3f753c67e8c21687a105c93ba945343
Leo Ribas <leonas.ribas@gmail.com>
2021-12-25 17:44:56 -0300

    ♻️ Refatora e transforma o TopTabNavigator
    Além da renomeação de TopTabNavigator para HomeStackNavigator, agora
    o componente de navegação é uma Stack

---
+++ src/navigators/HomeStackNavigator/index.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import BottomTabNavigator from "../BottomTabNavigator";
+import { createNativeStackNavigator, NativeStackScreenProps } from "@react-navigation/native-stack";
+import api from "../../services/api";
+import ActivityIndicatorBox from "../../components/ActivityIndicatorBox";
+
+type TopTabNavigatorProps = NativeStackScreenProps<RootStackParamList, "Home">;
+
+const Stack = createNativeStackNavigator();
+
+export default function HomeStackNavigator({ route }: TopTabNavigatorProps): React.ReactElement {
+    const [weekMenu, setWeekMenu] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        api.get("/week_menu")
+            .then((res) => {
+                setWeekMenu(res.data);
+                setLoading(false);
+            })
+            .catch((e) => {
+                console.log(e);
+                setLoading(false);
+            });
+    }, []);
+
+    return !loading ? (
+        <Stack.Navigator
+            screenOptions={{
+                header: (props) => <Header {...props} />,
+            }}
+        >
+            <Stack.Screen name={weekMenu[0].id}>
+                {(props) => <BottomTabNavigator {...props} dayMenu={weekMenu[0]} />}
+            </Stack.Screen>
+            <Stack.Screen name={weekMenu[1].id}>
+                {(props) => <BottomTabNavigator {...props} dayMenu={weekMenu[1]} />}
+            </Stack.Screen>
+            <Stack.Screen name={weekMenu[2].id}>
+                {(props) => <BottomTabNavigator {...props} dayMenu={weekMenu[2]} />}
+            </Stack.Screen>
+            <Stack.Screen name={weekMenu[3].id}>
+                {(props) => <BottomTabNavigator {...props} dayMenu={weekMenu[3]} />}
+            </Stack.Screen>
+            <Stack.Screen name={weekMenu[4].id}>
+                {(props) => <BottomTabNavigator {...props} dayMenu={weekMenu[4]} />}
+            </Stack.Screen>
+            <Stack.Screen name={weekMenu[5].id}>
+                {(props) => <BottomTabNavigator {...props} dayMenu={weekMenu[5]} />}
+            </Stack.Screen>
+            <Stack.Screen name={weekMenu[6].id}>
+                {(props) => <BottomTabNavigator {...props} dayMenu={weekMenu[6]} />}
+            </Stack.Screen>
+        </Stack.Navigator>
+    ) : (
+        <ActivityIndicatorBox />
+    );
+}
