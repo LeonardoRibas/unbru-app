031aa6756764ba1bade6491f5be8a647fd52861e
Leo Ribas <leoribasnasc@gmail.com>
2022-03-07 14:58:43 -0300

    🏷️ Adicona tipagem global para o cardápio do dia

--- declarations.d.ts
+++ declarations.d.ts
@@ -4,3 +4,10 @@ declare module "*.svg" {
     const content: React.FC<SvgProps>;
     export default content;
 }
+
+type TDayMenu = {
+    date: string;
+    breakfast: Record<string, unknown>;
+    lunch: Record<string, unknown>;
+    dinner: Record<string, unknown>;
+};
