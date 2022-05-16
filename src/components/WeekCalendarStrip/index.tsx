4cdbca80b2058a84cb688ca2d4326c45231adb65
Leo Ribas <leoribasnasc@gmail.com>
2022-05-16 04:34:52 -0300

    ♻️ Limpa estrutura do contexto

--- src/components/WeekCalendarStrip/index.tsx
+++ src/components/WeekCalendarStrip/index.tsx
@@ -1,17 +1,14 @@
 import styles from "./styles";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import api from "../../services/api";
 import { getDayOfWeek } from "../../utils/date";
 import { DayIndexContext } from "../../context/DayIndexContext";
 
-type WeekCalendarStripProps = {
-    date: Date;
-};
-
-export default function WeekCalendarStrip({ date }: WeekCalendarStripProps): React.ReactElement {
+export default function WeekCalendarStrip(): React.ReactElement {
     const [weekMenu, setWeekMenu] = useState([]);
+    const { dayIndex, setDayIndex } = useContext(DayIndexContext);
 
     useEffect(() => {
         api.get("/menu").then((res) => {
@@ -20,45 +17,41 @@ export default function WeekCalendarStrip({ date }: WeekCalendarStripProps): Rea
     }, []);
 
     return (
-        <DayIndexContext.Consumer>
-            {({ dayIndex, setDayIndex }) => (
-                <View style={styles.container}>
-                    <Text style={styles.title}>Outubro</Text>
-                    <ScrollView
-                        horizontal
-                        showsHorizontalScrollIndicator={false}
-                        showsVerticalScrollIndicator={false}
-                        fadingEdgeLength={10}
+        <View style={styles.container}>
+            <Text style={styles.title}>Outubro</Text>
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
+                fadingEdgeLength={10}
+            >
+                {weekMenu.map((day: TDayMenu, index) => (
+                    <TouchableOpacity
+                        key={day.date}
+                        style={styles.dayWrapper}
+                        onPress={() => setDayIndex(index)}
                     >
-                        {weekMenu.map((day: TDayMenu, index) => (
-                            <TouchableOpacity
-                                key={day.date}
-                                style={styles.dayWrapper}
-                                onPress={() => setDayIndex(index)}
+                        <Text style={styles.weekDayTitle}>{getDayOfWeek(day.date)}</Text>
+                        <View
+                            style={
+                                dayIndex === index
+                                    ? styles.selectedDayButtonContainer
+                                    : styles.dayButtonContainer
+                            }
+                        >
+                            <Text
+                                style={
+                                    dayIndex === index
+                                        ? styles.selectedDayButtonText
+                                        : styles.dayButtonText
+                                }
                             >
-                                <Text style={styles.weekDayTitle}>{getDayOfWeek(day.date)}</Text>
-                                <View
-                                    style={
-                                        dayIndex === index
-                                            ? styles.selectedDayButtonContainer
-                                            : styles.dayButtonContainer
-                                    }
-                                >
-                                    <Text
-                                        style={
-                                            dayIndex === index
-                                                ? styles.selectedDayButtonText
-                                                : styles.dayButtonText
-                                        }
-                                    >
-                                        {new Date(day.date).getDate()}
-                                    </Text>
-                                </View>
-                            </TouchableOpacity>
-                        ))}
-                    </ScrollView>
-                </View>
-            )}
-        </DayIndexContext.Consumer>
+                                {new Date(day.date).getDate()}
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                ))}
+            </ScrollView>
+        </View>
     );
 }
