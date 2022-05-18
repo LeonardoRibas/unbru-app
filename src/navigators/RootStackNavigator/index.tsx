af0fe64cc70f5c101b018c063911006a11067d05
Leo Ribas <leoribasnasc@gmail.com>
2022-05-18 01:55:35 -0300

    ♻️ Cria custom hook para a fech do menu

--- src/navigators/RootStackNavigator/index.tsx
+++ src/navigators/RootStackNavigator/index.tsx
@@ -1,29 +1,20 @@
+import React, { useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React, { useEffect, useState } from "react";
+import useFechMenu from "../../hooks/useFechMenu";
 import ActivityIndicatorBox from "../../components/ActivityIndicatorBox";
 import Header from "../../components/Header";
 import DayIndexContextProvider from "../../context/DayIndexContext";
 import OnBoarding from "../../pages/OnBoarding";
 import Settings from "../../pages/Settings";
-import api from "../../services/api";
 import { getFormatedDate } from "../../utils/date";
 import BottomTabNavigator from "../BottomTabNavigator";
 
 const Stack = createNativeStackNavigator();
 
 export default function RootStackNavigator(): React.ReactElement {
-    const [weekMenu, setWeekMenu] = useState<WeekMenu>();
+    const menu = useFechMenu();
     const [dayIndex, setDayIndex] = useState(0);
 
-    useEffect(() => {
-        api.get("/menu")
-            .then((res) => {
-                setWeekMenu(res.data);
-            })
-            .catch((e) => {
-                console.log(e);
-            });
-    }, []);
     return (
         <DayIndexContextProvider value={{ dayIndex, setDayIndex }}>
             <Stack.Navigator>
@@ -32,16 +23,16 @@ export default function RootStackNavigator(): React.ReactElement {
                     component={OnBoarding}
                     options={{ headerShown: false }}
                 />
-                {weekMenu ? (
+                {menu ? (
                     <Stack.Screen
                         name="Menu"
                         options={{
                             header: (props) => (
-                                <Header day={getFormatedDate(weekMenu[dayIndex].date)} {...props} />
+                                <Header day={getFormatedDate(menu[dayIndex].date)} {...props} />
                             ),
                         }}
                     >
-                        {(props) => <BottomTabNavigator {...props} dayMenu={weekMenu[dayIndex]} />}
+                        {(props) => <BottomTabNavigator {...props} dayMenu={menu[dayIndex]} />}
                     </Stack.Screen>
                 ) : (
                     <Stack.Screen name="Loading" component={ActivityIndicatorBox} />
