880b152fca6a0efb49a2e9b129ebbb6e8d60e93e
Leo Ribas <leonas.ribas@gmail.com>
2022-05-29 09:26:18 -0300

    🍱 Conserta custom icons

---
+++ src/components/CustomIcon/index.ts
@@ -0,0 +1,5 @@
+import { createIconSetFromIcoMoon } from "@expo/vector-icons";
+import selection from "../../../assets/icomoon/selection.json";
+
+const CustomIcon = createIconSetFromIcoMoon(selection, "IcoMoon", "icomoon.ttf");
+export default CustomIcon;
