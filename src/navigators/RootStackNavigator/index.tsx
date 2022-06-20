8dec150a1a1411b26b6786fd038a607c30dd04d6
Leo Ribas <leonas.ribas@gmail.com>
2022-06-19 22:35:37 -0300

    ✨ Faz a onboarding aparecer na 1 vez

--- src/navigators/RootStackNavigator/index.tsx
+++ src/navigators/RootStackNavigator/index.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import useFetchMenu from "../../hooks/useFetchMenu";
-import ActivityIndicatorBox from "../../components/ActivityIndicatorBox";
 import Header from "../../components/Header";
 import DayIndexContextProvider from "../../context/DayIndexContext";
 import OnBoarding from "../../pages/OnBoarding";
 import Settings from "../../pages/Settings";
 import BottomTabNavigator from "../BottomTabNavigator";
+import { checkIfFirstLaunch } from "../../utils/storage";
+import AppLoading from "expo-app-loading";
 
 const Stack = createNativeStackNavigator();
 
 export default function RootStackNavigator(): React.ReactElement {
     const [menu, setMenu] = useState<WeekMenu>([]);
     const [dayIndex, setDayIndex] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isFirstLaunch, setIsFirstLaunch] = useState<boolean>();
     const fetchMenu = useFetchMenu();
 
     async function setAndFetchMenu() {
@@ -21,18 +24,28 @@ export default function RootStackNavigator(): React.ReactElement {
     }
 
     useEffect(() => {
-        setAndFetchMenu();
+        async function setUp() {
+            const res = await checkIfFirstLaunch();
+            setIsFirstLaunch(res);
+            await setAndFetchMenu();
+            setIsLoading(false);
+        }
+        setUp();
     }, []);
 
     return (
         <DayIndexContextProvider value={{ menu, setMenu, dayIndex, setDayIndex }}>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="OnBoarding"
-                    component={OnBoarding}
-                    options={{ headerShown: false }}
-                />
-                {menu ? (
+            {isLoading ? (
+                <AppLoading />
+            ) : (
+                <Stack.Navigator>
+                    {isFirstLaunch ? (
+                        <Stack.Screen
+                            name="OnBoarding"
+                            component={OnBoarding}
+                            options={{ headerShown: false }}
+                        />
+                    ) : null}
                     <Stack.Screen
                         name="Menu"
                         options={{
@@ -41,17 +54,16 @@ export default function RootStackNavigator(): React.ReactElement {
                     >
                         {() => <BottomTabNavigator />}
                     </Stack.Screen>
-                ) : (
-                    <Stack.Screen name="Loading" component={ActivityIndicatorBox} />
-                )}
-                <Stack.Screen
-                    name="Settings"
-                    component={Settings}
-                    options={{
-                        title: "Configurações",
-                    }}
-                />
-            </Stack.Navigator>
+
+                    <Stack.Screen
+                        name="Settings"
+                        component={Settings}
+                        options={{
+                            title: "Configurações",
+                        }}
+                    />
+                </Stack.Navigator>
+            )}
         </DayIndexContextProvider>
     );
 }
---
+++ src/utils/storage.ts
@@ -0,0 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const HAS_LAUNCHED = "hasLaunched";
+
+function setAppLaunched() {
+    AsyncStorage.setItem(HAS_LAUNCHED, "true");
+}
+
+async function checkIfFirstLaunch() {
+    try {
+        const hasLaunched = await AsyncStorage.getItem(HAS_LAUNCHED);
+        if (hasLaunched === null) {
+            console.log("entrou");
+            setAppLaunched();
+            return true;
+        }
+        return false;
+    } catch (error) {
+        return false;
+    }
+}
+
+export { checkIfFirstLaunch };
