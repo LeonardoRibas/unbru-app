623c63f21ef7b38761489185a770b425b826d016
Leo Ribas <leoribasnasc@gmail.com>
2023-03-04 00:51:04 -0300

    ✨ Implementa estados com redux

--- src/navigators/RootStackNavigator/index.tsx
+++ src/navigators/RootStackNavigator/index.tsx
@@ -1,15 +1,37 @@
-import React, { useContext } from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Header from "../../components/Header";
 import OnBoarding from "../../pages/OnBoarding";
+import useFetchMenu from "../../hooks/useFetchMenu";
+import { useDispatch, useSelector } from "react-redux";
 import BottomTabNavigator from "../BottomTabNavigator";
-import SettingsStackNavigator from "../SettingsStackNavigator";
+import { setMenu } from "../../redux/features/menuSlice";
 import { GeneralContext } from "../../context/GeneralContext";
+import React, { useContext, useEffect, useState } from "react";
+import SettingsStackNavigator from "../SettingsStackNavigator";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
-export default function RootStackNavigator(): React.ReactElement {
-    const { isFirstLaunch, meal } = useContext(GeneralContext);
+export default function RootStackNavigator(): React.ReactElement | null {
+    const { isFirstLaunch } = useContext(GeneralContext);
+    const fetchMenu = useFetchMenu();
+    const meal = useSelector((state) => state.meal.value);
+    const dispatch = useDispatch();
+    const [menuReady, setMenuReady] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            fetchMenu()
+                .then((res) => {
+                    dispatch(setMenu(res));
+                })
+                .finally(() => setMenuReady(true));
+        };
+        fetchData();
+    }, []);
+
+    if (!menuReady) {
+        return null;
+    }
 
     return (
         <Stack.Navigator>
