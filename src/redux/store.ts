471f55f2298e9409a0bfc11123ffeb70244d6197
Leo Ribas <leoribasnasc@gmail.com>
2023-03-04 00:51:23 -0300

    ✨ Adiciona store

---
+++ src/redux/store.ts
@@ -0,0 +1,33 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import dayIndexReducer from "./features/dayIndexSlice";
+import menuReducer from "./features/menuSlice";
+import mealReducer from "./features/mealSlice";
+
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage: AsyncStorage,
+};
+
+const reducers = combineReducers({
+    dayIndex: dayIndexReducer,
+    menu: menuReducer,
+    meal: mealReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export default store;
