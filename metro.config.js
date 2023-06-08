fa450c48a549e459d975ccaf4ab135aee301f791
Leo Ribas <leoribasnasc@gmail.com>
2023-06-08 19:33:13 -0300

    🔧 Atualiza metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = (async () => {
-    const {
-        resolver: { sourceExts, assetExts },
-    } = await getDefaultConfig(__dirname);
-    return {
-        transformer: {
-            babelTransformerPath: require.resolve("react-native-svg-transformer"),
-        },
-        resolver: {
-            assetExts: assetExts.filter((ext) => ext !== "svg"),
-            sourceExts: [...sourceExts, "svg"],
-        },
+module.exports = (() => {
+    const config = getDefaultConfig(__dirname);
+
+    const { transformer, resolver } = config;
+
+    config.transformer = {
+        ...transformer,
+        babelTransformerPath: require.resolve("react-native-svg-transformer"),
     };
+    config.resolver = {
+        ...resolver,
+        assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+        sourceExts: [...resolver.sourceExts, "svg"],
+    };
+
+    return config;
 })();
