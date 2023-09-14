316b99da0feb02daf25e22a60600367bf09121f1
Leo Ribas <leoribasnasc@gmail.com>
2023-09-14 20:54:08 -0300

    ✨ Adds campus picker modal

--- src/navigators/SettingsStackNavigator/index.tsx
+++ src/navigators/SettingsStackNavigator/index.tsx
@@ -1,36 +1,66 @@
-import React from "react";
+import React, { useContext } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import About from "../../pages/About";
 import { View } from "react-native";
 import Option from "../../components/Option";
 import Feather from "@expo/vector-icons/build/Feather";
+import MaterialIcons from "@expo/vector-icons/build/MaterialCommunityIcons";
 import styles from "./styles";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import StackHeader from "../../components/StackHeader";
 import A2HSTutorial from "../../pages/A2HSTutorial";
+import Modal from "react-native-modal";
+import { GeneralContext } from "../../context/GeneralContext";
+import CampusPicker from "../../components/CampusPicker";
 
 type SettingsMenuProps = NativeStackScreenProps<SettingsStackParamList, "About">;
 
 const Stack = createNativeStackNavigator();
 
-const SettingsMenu = ({ navigation }: SettingsMenuProps) => (
-    <View style={styles.container}>
-        <Option
-            titleIcon={<Feather name="info" size={24} />}
-            title="Sobre"
-            subTitle="Saiba mais sobre o app"
-            actionIcon={<Feather name="chevron-right" size={24} />}
-            onPress={() => navigation.navigate("About")}
-        />
-        <Option
-            titleIcon={<Feather name="smartphone" size={24} />}
-            title="Tela inicial"
-            subTitle="Adicione o app à sua tela inicial"
-            actionIcon={<Feather name="chevron-right" size={24} />}
-            onPress={() => navigation.navigate("A2HSTutorial")}
-        />
-    </View>
-);
+const SettingsMenu = ({ navigation }: SettingsMenuProps) => {
+    const { isCampusSelectModalOpen, setIsCampusSelectModalOpen } = useContext(GeneralContext);
+
+    return (
+        <View style={styles.container}>
+            <Option
+                titleIcon={<MaterialIcons name="school-outline" size={24} />}
+                title="Campus"
+                subTitle="Troque o campus"
+                actionIcon={<Feather name="chevron-right" size={24} />}
+                onPress={() => setIsCampusSelectModalOpen(true)}
+            />
+            <Option
+                titleIcon={<Feather name="info" size={24} />}
+                title="Sobre"
+                subTitle="Saiba mais sobre o app"
+                actionIcon={<Feather name="chevron-right" size={24} />}
+                onPress={() => navigation.navigate("About")}
+            />
+            <Option
+                titleIcon={<Feather name="smartphone" size={24} />}
+                title="Tela inicial"
+                subTitle="Adicione o app à sua tela inicial"
+                actionIcon={<Feather name="chevron-right" size={24} />}
+                onPress={() => navigation.navigate("A2HSTutorial")}
+            />
+            <Modal
+                isVisible={isCampusSelectModalOpen}
+                onBackdropPress={() => setIsCampusSelectModalOpen(false)}
+                onSwipeComplete={() => setIsCampusSelectModalOpen(false)}
+                swipeDirection="down"
+                hideModalContentWhileAnimating
+                useNativeDriverForBackdrop
+                statusBarTranslucent
+                scrollHorizontal
+                propagateSwipe
+                backdropOpacity={0.5}
+                style={{ justifyContent: "flex-end", margin: 0 }}
+            >
+                <CampusPicker />
+            </Modal>
+        </View>
+    );
+};
 
 export default function SettingsStackNavigator(): React.ReactElement {
     return (
