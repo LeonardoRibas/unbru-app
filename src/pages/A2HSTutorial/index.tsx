b10082e89d5dc7273a5407f845478325dc35c906
Leo Ribas <leoribasnasc@gmail.com>
2023-10-29 20:05:00 -0300

    🔥 Remove tutorial de A2HS do android

--- src/pages/A2HSTutorial/index.tsx
+++ src/pages/A2HSTutorial/index.tsx
@@ -4,82 +4,7 @@ import styles from "./styles";
 import { Sizing } from "../../styles";
 import { Platform } from "react-native";
 
-const A2HSTutorial = Platform.select({
-    ios: () => <A2HSTutorialIOS />,
-    android: () => <A2HSTutorialIOS />,
-    web: () => <A2HSTutorialIOS />,
-});
-
-const A2HSTutorialAndroid = () => {
-    const { width } = Sizing.screen;
-
-    return (
-        <ScrollView style={styles.container}>
-            <View style={styles.step}>
-                <View style={{ flexDirection: "row", alignItems: "center" }}>
-                    <Text style={{ fontSize: 20 }}>1</Text>
-                    <Text> - Pressione o ícone indicado na figura:</Text>
-                </View>
-                <Image
-                    source={require("../../../assets/A2HSTutorial/android/a2hs1.png")}
-                    alt="Imagem mostrando o icone"
-                    style={{
-                        marginTop: 16,
-                        width: "100%",
-                        ...Platform.select({
-                            web: { maxWidth: 500 },
-                        }),
-                        height: 80,
-                        borderRadius: 16,
-                    }}
-                />
-            </View>
-            <View style={styles.step}>
-                <View style={{ flexDirection: "row", alignItems: "center" }}>
-                    <Text style={{ fontSize: 20 }}>2</Text>
-                    <Text> - Selecione a opção “Adicionar à tela principal”</Text>
-                </View>
-                <View style={{ borderRadius: 16 }}>
-                    <Image
-                        source={require("../../../assets/A2HSTutorial/android/a2hs2.png")}
-                        alt="asdf"
-                        style={{
-                            marginTop: 16,
-                            width: "100%",
-                            ...Platform.select({
-                                web: { maxWidth: 500, maxHeight: 500 * (16 / 9) },
-                            }),
-                            height: width * (16 / 9),
-                            borderRadius: 16,
-                        }}
-                    />
-                </View>
-            </View>
-            <View style={styles.step}>
-                <View style={{ flexDirection: "row", alignItems: "center" }}>
-                    <Text style={{ fontSize: 20 }}>3</Text>
-                    <Text> - Em seguida em “Adicionar”</Text>
-                </View>
-
-                <Image
-                    source={require("../../../assets/A2HSTutorial/android/a2hs3.png")}
-                    alt="asdf"
-                    style={{
-                        marginTop: 16,
-                        width: "100%",
-                        ...Platform.select({
-                            web: { maxWidth: 500, maxHeight: 500 * (16 / 9) },
-                        }),
-                        height: width * (16 / 9),
-                        borderRadius: 16,
-                    }}
-                />
-            </View>
-        </ScrollView>
-    );
-};
-
-const A2HSTutorialIOS = () => {
+const A2HSTutorialIOS = (): React.ReactElement => {
     const { width } = Sizing.screen;
 
     return (
@@ -148,4 +73,4 @@ const A2HSTutorialIOS = () => {
     );
 };
 
-export default A2HSTutorial;
+export default A2HSTutorialIOS;
