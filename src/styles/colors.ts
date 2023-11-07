e02c76de1e494d4f80ea4dcde37bc94298d9d2ad
Leo Ribas <leoribasnasc@gmail.com>
2023-11-07 00:57:45 -0300

    💄 Atualiza cor primaria

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -13,45 +13,14 @@ export const neutral = {
 };
 
 export const primary = {
-    s300: "#91D499",
-    s400: "#6DC577",
-    base: "#48B755",
-    s600: "#3A9244",
-    s700: "#2B6E33",
-};
-
-export const success = {
-    s100: "#DBF0DE",
-    s200: "#B6E2BD",
-    s300: "#92D39C",
-    s400: "#6EC47C",
-    base: "#4AB55B",
-    s600: "#3B9149",
-    s700: "#2C6D36",
-    s800: "#1D4924",
-    s900: "#0F2412",
-};
-
-export const warning = {
-    s100: "#FCEECF",
-    s200: "#F8DDA0",
-    s300: "#F5CC70",
-    s400: "#F2BC40",
-    base: "#EEAB11",
-    s600: "#BF890D",
-    s700: "#8F660A",
-    s800: "#5F4407",
-    s900: "#302203",
-};
-
-export const error = {
-    s100: "#F8D3D8",
-    s200: "#F2A6B1",
-    s300: "#EB7A8A",
-    s400: "#E54D63",
-    base: "#DE213C",
-    s600: "#B21A30",
-    s700: "#851424",
-    s800: "#590D18",
-    s900: "#2C070C",
+    s050: "#E9FBF2",
+    s100: "#D3F7E4",
+    s200: "#A8F0CA",
+    s300: "#7DE8AF",
+    s400: "#52E095",
+    base: "#27D87B",
+    s600: "#20AC63",
+    s700: "#18814A",
+    s800: "#105632",
+    s900: "#082B19",
 };
