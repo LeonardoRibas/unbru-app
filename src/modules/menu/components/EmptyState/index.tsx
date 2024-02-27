419949c4a81d8a54d2bd881c6b4feef656aa9e3f
atmoszito <160150644+atmoszito@users.noreply.github.com>
2024-02-26 21:58:16 -0300

    ✨ Adiciona darkmode ao emptystate

--- src/modules/menu/components/EmptyState/index.tsx
+++ src/modules/menu/components/EmptyState/index.tsx
@@ -2,12 +2,18 @@ import { View, Text, Platform } from "react-native";
 import React from "react";
 import styles from "./styles";
 import Test from "assets/icons/fluent-emoji-flat_fork-and-knife-with-plate.svg";
+import useAppSelector from "@modules/common/hooks/useAppSelector";
+import { Theme } from "src/styles";
 
 const EmptyState = (): React.ReactElement => {
+    const theme = useAppSelector((state) => state.theme);
+
     return (
-        <View style={styles.container}>
+        <View style={[styles.container, { backgroundColor: Theme[theme].background_default }]}>
             {Platform.OS === "web" ? <img src={Test}></img> : <Test width={150} height={150} />}
-            <Text style={styles.title}>Cardápio Indisponível</Text>
+            <Text style={[styles.title, { color: Theme[theme].text_secondary }]}>
+                Cardápio Indisponível
+            </Text>
         </View>
     );
 };
